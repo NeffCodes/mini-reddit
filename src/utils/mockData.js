52d05170d8c91cacc64ae5d87190aba56b7fccae
Persigio <sparkraven@gmail.com>
2021-07-13 16:21:20 -0400

    Add React Icons and mockData

---
+++ src/utils/mockData.js
@@ -0,0 +1,20 @@
+export const mockPostData = [
+  {
+    subreddit: 'ExampleSubreddit',
+    subreddit_name_prefixed: 'r/ExampleSubreddit',
+    title: 'Example title',
+    timeStamp: 2,
+    author: 'Example Author',
+    thumbnail: 'https://via.placeholder.com/96',
+    num_comments: 251,
+  },
+  {
+    subreddit: 'AnotherSubreddit',
+    subreddit_name_prefixed: 'r/AnotherSubreddit',
+    title: 'Another title',
+    timeStamp: 5,
+    author: 'Another Author',
+    thumbnail: 'https://via.placeholder.com/96',
+    num_comments: 23,
+  },
+];
