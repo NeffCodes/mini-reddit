89f96503509c1535454c48235d2d80d99738741d
Persigio <sparkraven@gmail.com>
2021-07-13 16:03:33 -0400

    Displays posts data in PostFeed

--- src/features/Post/PostFeed.jsx
+++ src/features/Post/PostFeed.jsx
@@ -1,12 +1,22 @@
 import { useEffect } from 'react';
+import Post from './Post';
 
-export const PostFeed = ({ loadPosts }) => {
+export const PostFeed = ({ loadPosts, posts }) => {
   useEffect(() => {
     loadPosts();
   }, [loadPosts]);
 
   return (
+    <section>
       <h2>Post Feed</h2>
+      <ul>
+        {posts.map((post, index) => (
+          <li key={index} data-testid={post.subreddit}>
+            {post.subreddit}
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 };
 
--- src/features/___tests___/PostFeed.spec.js
+++ src/features/___tests___/PostFeed.spec.js
@@ -1,12 +1,24 @@
 import { render } from '@testing-library/react';
 import { PostFeed } from '../Post/PostFeed';
 
+import { mockPostData } from '../../utils/mockData';
+
 describe('Post Feed', () => {
+  const posts = mockPostData;
+  let loadPosts;
+  let context;
+
+  beforeEach(() => {
+    loadPosts = jest.fn().mockName('loadPosts');
+    context = render(<PostFeed posts={posts} loadPosts={loadPosts} />);
+  });
+
   it('loads posts on first render', () => {
-    const loadPosts = jest.fn().mockName('loadPosts');
-
-    render(<PostFeed loadPosts={loadPosts} />);
-
     expect(loadPosts).toHaveBeenCalled();
+  });
+  it('displays the posts', () => {
+    const { queryByTestId } = context;
+    expect(queryByTestId('ExampleSubreddit')).not.toBeNull();
+    expect(queryByTestId('AnotherSubreddit')).not.toBeNull();
   });
 });
