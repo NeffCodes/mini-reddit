9159f0f9325fc18c3037801700610b0771fc4305
Persigio <sparkraven@gmail.com>
2021-07-27 13:54:43 -0400

    Setup post body section
    TODO: build/integrate a photo gallery system

---
+++ src/features/Post/body/postBody.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import { fixedString } from '../../../utils/fixString';
+import { ImLink } from 'react-icons/im';
+import bodyStyles from './postBody.module.css';
+
+export const PostBody = ({ content, i }) => {
+  const postData = content.data;
+  const displaySubText = sub => {
+    return sub.selftext && sub.thumbnail !== 'spoiler' ? true : false;
+  };
+
+  return (
+    <div>
+      <div>
+        <h3 className={bodyStyles.title}>
+          {i + ': ' + fixedString(postData.title)}
+        </h3>
+
+        {/*Displays secondary text if provided*/}
+        {displaySubText(postData) && (
+          <p className={bodyStyles.selftext}>
+            {fixedString(postData.selftext)}
+          </p>
+        )}
+
+        {/* Displays link if provided */}
+        {postData.post_hint === 'link' && (
+          <a href={postData.url_overridden_by_dest} className={bodyStyles.link}>
+            <ImLink /> {`https://${postData.domain}`}
+          </a>
+        )}
+      </div>
+
+      <div className={bodyStyles.media}>
+        {/* Displays single image if provided */}
+        {postData.post_hint === 'image' && (
+          <img src={postData.url_overridden_by_dest} alt="post media" />
+        )}
+
+        {/* TODO: Displays gallery images if provided */}
+        {postData.is_gallery && <img src="" alt="WIP Gallery" />}
+
+        {/* Displays reddit hosted video if provided */}
+        {postData.is_video && (
+          <div>
+            <video preload="auto" controls>
+              <source src={postData.media.reddit_video.fallback_url} />
+              <p>
+                Your browser doesn't surpport HTML5 video. You can view it on
+                <a href={postData.permalink}>Reddit's post</a>
+                instead
+              </p>
+            </video>
+          </div>
+        )}
+
+        {/* Displays outside reddit videwo if provided */}
+        {postData.post_hint === 'rich:video' && (
+          <div>
+            <img
+              src={fixedString(postData.secure_media.oembed.thumbnail_url)}
+              alt={postData.secure_media.oembed.title}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
--- src/utils/fixString.jsx
+++ src/utils/fixString.jsx
@@ -6,5 +6,7 @@ export const fixedString = str => {
   str = str.replace(/&amp;/g, '&');
   str = str.replace(/&lt;/g, '<');
   str = str.replace(/&gt;/g, '>');
+  str = str.replace(/\\/g, '');
+  str = str.replace(/&#x200B;/g, ' ');
   return str;
 };
