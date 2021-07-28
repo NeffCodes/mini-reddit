7c8072957df019841147aa150988dd298af6c5ca
Persigio <sparkraven@gmail.com>
2021-07-28 16:45:00 -0400

    Setup AsideSubreddit

--- src/features/App/App.js
+++ src/features/App/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 
 import PostFeed from '../Post/PostFeed';
+import AsideSubs from '../Subreddits/AsideSubreddit';
 
 function App() {
   return (
     <>
-      <PostFeed />
+      <main>
+        <PostFeed className="list" />
+        <AsideSubs className="side" />
+      </main>
     </>
   );
 }
---
+++ src/features/Subreddits/AsideSubreddit.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from './AsideSubreddit.module.css';
+export const AsideSubs = () => {
+  return (
+    <aside className={styles.container}>
+      <h2>Sub List</h2>
+    </aside>
+  );
+};
+
+export default AsideSubs;
